8fc81db7a7d59a1dcadb95ba70c2c0f1fa42d48f
Emily Lo <elo88@comcast.net>
2022-04-18 14:46:38 -0400

    datatabase of quiz questions

---
+++ quizServer.py
@@ -0,0 +1,52 @@
+quiz = [
+    {
+        "id": 1,
+        "question": Pupin hall (office) is approximately 15 stories high, how many meters tall is it?
+        "imglink": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Columbia_University_-_Department_of_Physics_%2848170362276%29.jpg/250px-Columbia_University_-_Department_of_Physics_%2848170362276%29.jpg",
+        "op1": "15",
+        "op2": "40",
+        "op3": "73",
+        "op4": "97",
+        "correct": "73"
+    },
+    {
+        "id": 2,
+        "question": Low Memorial Library (misc) is approximately 4 stories high, hom many meters tall is it?
+        "imglink": "https://upload.wikimedia.org/wikipedia/commons/5/5d/Low_Memorial_Library_Columbia_University_College_Walk_Court_Yard_05.jpg",
+        "op1": "13",
+        "op2": "24",
+        "op3": "35",
+        "op4": "46",
+        "correct": "24"
+    },
+    {
+        "id": 3,
+        "question": Lerner Hall (misc) is approximately 10 stories high, hom many meters tall is it?
+        "imglink": "https://lernerhall.columbia.edu/files/lerner/content/lerner-night-rev.gif",
+        "op1": "15",
+        "op2": "46",
+        "op3": "53",
+        "op4": "64",
+        "correct": "46"
+    },
+    {
+        "id": 4,
+        "question": Pulitzer hall (office) is approximately 9 stories high, how many meters tall is it?
+        "imglink": "https://www.wikicu.com/images/thumb/b/b9/Journalism.jpg/240px-Journalism.jpg",
+        "op1": "40",
+        "op2": "46",
+        "op3": "49",
+        "op4": "72",
+        "correct": "49"
+    },
+    {
+        "id": 5,
+        "question": Fayerweather hall (office) is approximately 10 stories high, how many meters tall is it?
+        "imglink": "https://admissionsblog.sipa.columbia.edu/wp-content/uploads/2017/02/tour-fayerweather8.png",
+        "op1": "38",
+        "op2": "44",
+        "op3": "46",
+        "op4": "79",
+        "correct": "44"
+    }
+]
